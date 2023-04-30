df009aa30814eb0057987a4d98009725b025601e
Aryan-Obrai <hobra001@ucr.edu>
2023-04-30 03:36:54 -0700

    Added special case to handle spaces in "City"

--- scripts/script.js
+++ scripts/script.js
@@ -2,18 +2,26 @@ const form = document.querySelector('form');
 const cityInput = document.querySelector('#city-input');
 const submitBtn = document.querySelector('#submit-btn');
 
+function capitalize(str) {
+    return str
+      .split(' ')
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+  }
+
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  const city = cityInput.value;
-  if (city.trim() === '') {
+  let city = cityInput.value.trim().toLowerCase();
+  if (city === '') {
     alert('Please enter a city name');
     return;
   }
+  city = capitalize(city);
   
   fetch('/data/data.json')
     .then(response => response.json())
     .then(json => {
-      if (json[city]) {
+      if (json.hasOwnProperty(city)) {
         alert(`${city} is ${json[city]}`);
       } else {
         alert(`${city} is not in our database`);
@@ -24,9 +32,3 @@ form.addEventListener('submit', (event) => {
       alert('Failed to load safety scores');
     });
 });
-
-function getData() {
-    fetch("/data/data.json")
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-}
